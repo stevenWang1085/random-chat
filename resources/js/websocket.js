efa9d5a0570ec0ac7db95ae3f5f7a9ee826034a4
steven <4a114019@stust.edu.tw>
2023-08-16 14:14:57 +0800

    modify env

--- resources/js/websocket.js
+++ resources/js/websocket.js
@@ -15,7 +15,7 @@ window.Echo = new Echo({
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    wssPort: import.meta.env.CLIENT_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
     encrypted: true,
