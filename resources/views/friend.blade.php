1a5bbc206d81f69cde48ffe92c43a40b34fdf57c
steven <4a114019@stust.edu.tw>
2023-08-17 11:30:48 +0800

    new icon

--- resources/views/friend.blade.php
+++ resources/views/friend.blade.php
@@ -2,22 +2,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>friend list</title>
+    <title>聊聊-好友清單</title>
+    <link rel="icon" href="{{ asset('/image/chat.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-{{--    <link--}}
-{{--        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"--}}
-{{--        rel="stylesheet"--}}
-{{--    />--}}
-    <!-- Google Fonts -->
-{{--    <link--}}
-{{--        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"--}}
-{{--        rel="stylesheet"--}}
-{{--    />--}}
-{{--    <!-- MDB -->--}}
-{{--    <link--}}
-{{--        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.css"--}}
-{{--        rel="stylesheet"--}}
-{{--    />--}}
     @vite('resources/js/friend.js')
 </head>
 <body id="friend" class="friend" style="background-color: #eee; padding-top: 56px">
--- resources/views/friend_check.blade.php
+++ resources/views/friend_check.blade.php
@@ -2,7 +2,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>friend check</title>
+    <title>聊聊-審核列表</title>
+    <link rel="icon" href="{{ asset('/image/chat.ico') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     @vite('resources/js/friend_check.js')
 </head>
